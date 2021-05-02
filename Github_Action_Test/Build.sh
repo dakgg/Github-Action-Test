08ff0a405a0fc5c73b8339c5a9137f395be1fe7b
hs.hwang <hs.hwang@smartstudygames.com>
2021-05-02 16:49:33 +0900

    no message

--- Github_Action_Test/Build.sh
+++ Github_Action_Test/Build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 
-echo haha
 "C:\Program Files\Unity\Hub\Editor\2020.2.5f1\Editor\Unity.exe" \
 -projectPath "C:\actions-runner\_work\Github-Action-Test\Github-Action-Test" \
 -batchmode -quit \
